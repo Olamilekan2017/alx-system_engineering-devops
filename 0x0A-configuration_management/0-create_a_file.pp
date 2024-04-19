890f4b5177f927ef161c5bbc1afaea1c37956531
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-04-19 10:47:44 -0700

    I love Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Puppet manifest for creating a file in /tmp with specific permissions, owner, group, and content
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
